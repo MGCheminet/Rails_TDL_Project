ef15c29b687e427008dffbe752b03bbb8a8923c5
Melina Cheminet <mcheminet@fi.uba.ar>
2023-06-17 09:40:49 -0300

    Cambios varios

--- fairshare/app/controllers/grupos_controller.rb
+++ fairshare/app/controllers/grupos_controller.rb
@@ -12,20 +12,6 @@ class GruposController < ApplicationController
   end
   
 
-#  def create
-#    @grupo = Grupo.new(grupo_params)
-#    @grupo.created_by = current_user.id
-#  
-#    if @grupo.save
-#      @grupo.usuarios << current_user 
-#      flash[:notice] = 'Grupo creado exitosamente.'
-#      redirect_to grupos_path
-#    else
-#      flash.now[:alert] = 'Error al crear el grupo.'
-#      render :new
-#    end
-#  end
-
   def create
     @grupo = Grupo.new(grupo_params)
     @grupo.created_by = current_user.id
@@ -54,14 +40,55 @@ class GruposController < ApplicationController
     @usuarios_grupo = @grupo.usuarios
   end
 
+  #def update
+  #  if @grupo.update(grupo_params)
+  #    redirect_to @grupo, notice: 'Grupo actualizado exitosamente.'
+  #  else
+  #    @usuarios = Usuario.where.not(id: current_user.id)
+  #    render :edit
+  #  end
+  #end
+
   def update
+    usuario_ids = params[:grupo][:id_usuarios] || []
+  
     if @grupo.update(grupo_params)
+      modificar_usuarios(usuario_ids)
       redirect_to @grupo, notice: 'Grupo actualizado exitosamente.'
     else
       @usuarios = Usuario.where.not(id: current_user.id)
       render :edit
     end
   end
+  
+  def modificar_usuarios(usuario_ids)
+    # Find the existing users associated with the group
+    existing_users = @grupo.usuarios.to_a
+    
+    # Add the creator of the group if not already in the group
+    @grupo.usuarios << current_user unless existing_users.include?(current_user)
+    
+    # Iterate over the selected user IDs
+    usuario_ids.each do |usuario_id|
+      usuario = Usuario.find(usuario_id)
+      
+      # Add the user to the group if they are not already associated
+      @grupo.usuarios << usuario unless existing_users.include?(usuario)
+      
+      # Remove the user from the existing users list to track which users should remain associated
+      existing_users.delete(usuario)
+    end
+    
+    # Remove any remaining users from the group who were unchecked
+    existing_users.each do |usuario|
+      @grupo.usuarios.delete(usuario)
+    end
+  end
+  
+    
+  
+  
+
 
   def destroy
     if @grupo.destroy
@@ -85,15 +112,15 @@ class GruposController < ApplicationController
   def set_grupo
     @grupo = Grupo.find(params[:id])
   end
-  x
+  
 
-  def usuario_agregar
-    usuario_ids = params[:grupo][:id_usuarios] || []
-    usuario_ids.each do |usuario_id|
-      @grupo.usuarios << Usuario.find(usuario_id)
-    end
-    redirect_to @grupo, notice: 'Usuarios agregados al grupo exitosamente.'
-  end
+  #def usuario_agregar
+  #  usuario_ids = params[:grupo][:id_usuarios] || []
+  #  usuario_ids.each do |usuario_id|
+  #    @grupo.usuarios << Usuario.find(usuario_id)
+  #  end
+  #  redirect_to @grupo, notice: 'Usuarios agregados al grupo exitosamente.'
+  #end
   
 
   def grupo_params
