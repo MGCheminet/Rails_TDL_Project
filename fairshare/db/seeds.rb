849d4cc468710deb9af7bb4fb1e1ee1bfc1bd8ea
AgusOrma <50749047+AgusOrma@users.noreply.github.com>
2023-06-19 10:56:27 -0300

    Semilla del admin realizada

--- fairshare/db/seeds.rb
+++ fairshare/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+admin_user = Usuario.new(nombre: 'admin', password: 'password', email: 'admin@example.com', admin: true)
+admin_user.save
