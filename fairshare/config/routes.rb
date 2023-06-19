f8f98c3da7934647494fade1da8c592a7e9fe444
AgusOrma <50749047+AgusOrma@users.noreply.github.com>
2023-06-19 10:05:05 -0300

    ruta de dividir gastos subida

--- fairshare/config/routes.rb
+++ fairshare/config/routes.rb
@@ -2,27 +2,20 @@ Rails.application.routes.draw do
   # Configura las rutas RESTful para el controlador Usuarios y automáticamente asigna las rutas a las acciones del controlador 
   
   post '/grupos/:id/agregar_usuario', to: 'grupos#agregar_usuario', as: :agregar_usuario
+  
   get 'signup', to: 'usuarios#new'
   get 'login', to: 'sesiones#new', as: :login
   get 'logout', to: 'sesiones#destroy', as: :logout
   # post 'login', to: 'sesiones#create', as: :sesiones
   get '/home', to: 'home#index', as: :index_home
   get 'usuarios/:id/gastos', to: 'gastos#index', as: :mis_gastos
-
   
-  resources :grupos do
-    collection do
-      get 'admin_grupos', to: 'grupos#admin_grupos'
-    end
-  end
   
   resources :usuarios do
+    resources :gastos, only: [:create]
     member do
       delete :delete_self
-      put :block, action: :block_usuario
-      put :unblock, action: :unblock_usuario
     end
-    resources :gastos, only: [:create]
   end
 
   get '/usuarios/:id', to: 'usuarios#show', as: :perfil
@@ -39,9 +32,13 @@ Rails.application.routes.draw do
   resources :grupos do
     resources :gastos, only: [:new, :create]  
     post 'agregar_usuarios', on: :member
-    post 'dividir_gastos', on: :member
+    #post 'dividir_gastos', on: :member
   end
-  
+
+  #Agregado por AGUSTIN
+  get 'grupos/:grupo_id/dividir_gastos', to: 'divisiones#show', as: 'dividir_gastos_grupo'
+
+
   root 'inicio#index'  # Establece la página de inicio
 end
 
